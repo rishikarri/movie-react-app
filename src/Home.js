1436d96983243ef9154985443f1f46043a5a3978
Rishi Karri <rishikarri1@gmail.com>
2017-01-16 17:03:44 -0500

    added buttons and links to categories

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,23 @@ import $ from 'jquery';
 
 //custom component/modules
 import Poster from './Poster';
+import Constants from './Constants';
+import Config from './Config';
+import DiscoverButton from './DiscoverButton';
 
 import './App.css';
 
 class Home extends Component{
 	constructor(props){
 		super(props); 
+		this.handleCategoryChange = this.handleCategoryChange.bind(this);
 		this.state = {
 			moviePosters: []
 		}
 	}
 	
-    componentDidMount() {
-        var url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5';
+    componentDidMount() {        
+        var url = Constants.baseUrl + Constants.nowPlayingEP + Config.apiKey;
         $.getJSON(url, (movieData)=>{
             this.setState({
                 moviePosters: movieData.results
@@ -24,14 +28,41 @@ class Home extends Component{
         })
     }	
 
+    handleCategoryChange(categoryApiUrl){
+        	var url = Constants.baseUrl + categoryApiUrl + Config.apiKey; 
+        	var self = this;
+        	console.log(url);
+
+        	$.getJSON(url, (categoryData)=>{
+        		this.setState({
+        			moviePosters: categoryData.results
+        		})
+        	})
+     }
+
+
+
 	render(){
         var postersArray = [];
         this.state.moviePosters.map((currentPoster, index) =>{
             postersArray.push(<Poster poster={currentPoster} key={index} />)
         });
+        // custom function to update home's state var, FROM THE CHILD BUTTON
+        
+
+        // build buttons with discover buttons component
+        var discoverButtons = [];
+        Constants.discoverLinks.map((category, index)=>{
+        	discoverButtons.push(
+        		<DiscoverButton buttonLink={category.link} functionFromParent={this.handleCategoryChange} buttonText={category.buttonText} key={index} />
+        	)
+
+        })
 		return(
 			<div>
+				{discoverButtons}
 				{postersArray}
+				}
 			</div>
 		)
 	}
